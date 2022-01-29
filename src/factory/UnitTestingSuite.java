40b7f549e7de25e1440d327b77235f43dc4e4aca
Alex Garcia Amezcua <alex.garciaa@epp.urv.cat>
2022-01-29 22:16:58 +0100

    Merged all changes

--- src/factory/UnitTestingSuite.java
+++
@@ -1,8 +0,0 @@
-package factory;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
-
-@RunWith(value=Suite.class)
-@SuiteClasses(value={UnitTesting.class})
-public class UnitTestingSuite {}
